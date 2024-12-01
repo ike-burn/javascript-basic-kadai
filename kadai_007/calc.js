17d53919df529a35b6fc95797295778819d7cbb3
ike-burn <113594151+ike-burn@users.noreply.github.com>
2024-12-01 23:51:59 +0900

    kadai_007
    kadai_007をアップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,17 @@
+let num = 15;
+
+if(num === 3) {
+	console.log('３の倍数です');
+}
+
+else if(num === 5) {
+	console.log('５の倍数です');
+}
+
+else if(num === 3 || num ===5) {
+	console.log('３と５の倍数です');
+}
+
+else {
+	console.log(num);
+}
